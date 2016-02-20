b74cb277b18bea138a1addfb72ef5f31114e86d8
Karl Stolley <karl.stolley@gmail.com>
2016-02-20 16:38:36 -0600

    Nuke contents of site.js, minus opening comment

--- html/js/site.js
+++ html/js/site.js
@@ -1,13 +1,1 @@
 //Site JavaScript
-
-$(document).ready(function() {
-  $('body').addClass('hasjs');
-  rpkwidescreen();
-  $(window).resize(rpkwidescreen);
-  });
-
-function rpkwidescreen() {
-    var rpkwidth = $(window).width();
-    if(rpkwidth>1100) { $('body').addClass('widescreen'); }
-    else { $('body').removeClass('widescreen'); }
-}
